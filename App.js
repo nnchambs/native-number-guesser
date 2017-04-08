366947565363c2e9e9588fa9fd94977a36101d2f
Nick <nicknchambers@gmail.com>
2017-04-07 18:37:23 -0600

    adds random number generator, text input, and submit button

--- App.js
+++ App.js
@@ -1,13 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      randomNumber: '',
+      guessedNumber: ''
+    }
+  }
+
+  componentDidMount() {
+    this.setState({randomNumber: Math.floor(Math.random() * 100)})
+  }
+
+  setGuessedNumber(guessedNumber) {
+    this.setState({guessedNumber: guessedNumber})
+  }
+
+  compareNumbers() {
+
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>{this.state.randomNumber ? this.state.randomNumber : 'Loading'}</Text>
+        <Text>Guess a number between 1 and 100 :-P</Text>
+        <TextInput
+          style={{height: 40, width: 60, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={({text}) => this.setGuessedNumber({text})}
+          >
+
+        </TextInput>
+        <Button title="Submit" onPress={() => this.compareNumbers()}></Button>
+        <Text></Text>
       </View>
     );
   }
